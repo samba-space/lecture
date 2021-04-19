4094b89e6b2895a9c9bf1dbfee378af8554d8472
Park <junki.park@lgcns.com>
2021-04-19 21:08:21 +0900

    member Test code

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,14 @@
+package hello.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberApp {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
