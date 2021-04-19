2d5ce0cac694ae89c72627e8af96afdc88e79cc5
Samba <junkipar430@gmail.com>
2021-04-19 21:16:54 +0900

    push test

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -12,6 +12,7 @@ public class MemberApp {
 		MemberService memberService = new MemberServiceImpl();
 		Member member = new Member(1L, "memberA", Grade.VIP);
 		memberService.join(member);
+		
 		Member findMember = memberService.findMember(1L);
 		System.out.println("new member = " + member.getName());
 		System.out.println("find Member = " + findMember.getName());
