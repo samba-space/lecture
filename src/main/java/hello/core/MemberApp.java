f339985954e3b3a2152c6bc3e43bd3ee0b9de38b
Park <junki.park@lgcns.com>
2021-04-19 21:10:27 +0900

    테스트 코드

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -1,14 +1,13 @@
 package hello.core;
 
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class MemberApp {
-
 	public static void main(String[] args) {
-
-
-	}
-
-}
+		MemberService memberService = new MemberServiceImpl(); Member member = new Member(1L, "memberA", Grade.VIP); memberService.join(member);
+		Member findMember = memberService.findMember(1L); System.out.println("new member = " + member.getName()); System.out.println("find Member = " + findMember.getName());
+	} }
