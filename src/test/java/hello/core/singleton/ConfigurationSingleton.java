556d84ea3fa5833cf8b275f29e3fd10951795b95
Samba <junkipark430@gmail.com>
2021-04-29 17:25:48 +0900

    SingletonTest 추가

--- src/test/java/hello/core/singleton/ConfigurationSingleton.java
+++ src/test/java/hello/core/singleton/ConfigurationSingleton.java
@@ -1,6 +1,7 @@
 package hello.core.singleton;
 
 import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class ConfigurationSingleton {
 
     @Test
+    @DisplayName("빈 조회")
     void configurationDeep() {
         ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
