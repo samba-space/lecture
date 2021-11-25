91566fdc523fc0d2e9e4d4bb4b4a66e126ffbaa6
Samba <junkipark430@gmail.com>
2021-11-25 23:20:07 +0900

    프록시 패턴, 데코레이터 패턴 예제

--- 스프링핵심원-고급편/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ 스프링핵심원-고급편/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
+import java.io.FilenameFilter;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.DoubleToIntFunction;
+import java.util.function.Function;
+
 @Import({AppV1Config.class, AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
