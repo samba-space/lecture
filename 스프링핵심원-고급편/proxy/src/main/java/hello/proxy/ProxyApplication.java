11a4497f8ee71e25294cbc6e6dcaaf79cd56e093
Samba <junkipark430@gmail.com>
2021-12-12 22:53:32 +0900

    프록시 팩토리

--- 스프링핵심원-고급편/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ 스프링핵심원-고급편/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -4,6 +4,8 @@ import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v2_dynamicproxy.handler.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicproxy.handler.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -19,7 +21,9 @@ import java.util.function.Function;
 
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
+//@Import(ConcreteProxyConfig.class)
+//@Import(DynamicProxyBasicConfig.class)
+@Import(DynamicProxyFilterConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
