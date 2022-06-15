02c4ba99b3c38c142250ca8441ac9eda77dc862a
Samba <junkipark430@gmail.com>
2022-06-16 00:06:03 +0900

    validation

--- spring/스프링MVC2/project/validation/src/main/java/hello/itemservice/web/validation/form/ItemUpdateForm.java
+++ spring/스프링MVC2/project/validation/src/main/java/hello/itemservice/web/validation/form/ItemUpdateForm.java
@@ -1,30 +1,24 @@
-package hello.itemservice.domain.item;
+package hello.itemservice.web.validation.form;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
-import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
-public class Item {
+public class ItemUpdateForm {
 
+    @NotNull
     private Long id;
 
+    @NotBlank
     private String itemName;
 
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
 
     private Integer quantity;
-
-    public Item() {
-    }
-
-    public Item(String itemName, Integer price, Integer quantity) {
-        this.itemName = itemName;
-        this.price = price;
-        this.quantity = quantity;
-    }
 }
