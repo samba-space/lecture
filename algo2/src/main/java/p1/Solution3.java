fbbb2cf02f5522693322bce4f3d8d24c1065c7da
samba-space <junkipark430@gmail.com>
2022-08-31 00:14:36 +0900

    알고리즘 풀이

---
+++ algo2/src/main/java/p1/Solution3.java
@@ -0,0 +1,38 @@
+package p1;
+
+import java.util.Scanner;
+
+public class Solution3 {
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = in.nextInt();
+        }
+        int solution = solution(n, k, a);
+
+        System.out.println(solution);
+
+        return;
+    }
+
+    public static int solution(int n, int k, int[] arr) {
+        int max;
+        int windowSum = 0;
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
+        max = windowSum;
+
+        for (int i = k; i < n; i++) {
+            windowSum = windowSum + arr[i] - arr[i - k];
+            max = Math.max(windowSum, max);
+        }
+
+        return max;
+    }
+}
