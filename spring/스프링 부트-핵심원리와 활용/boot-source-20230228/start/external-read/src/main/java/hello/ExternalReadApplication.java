b15ba91972bcafabc9e1807e0fe28a52fcd9bef5
samba-space <junkipark430@gmail.com>
2023-07-09 00:39:11 +0900

    config2

--- spring/스프링 부트-핵심원리와 활용/boot-source-20230228/start/external-read/src/main/java/hello/ExternalReadApplication.java
+++ spring/스프링 부트-핵심원리와 활용/boot-source-20230228/start/external-read/src/main/java/hello/ExternalReadApplication.java
@@ -1,9 +1,14 @@
 package hello;
 
+import hello.config.MyDataSourceValueConfig;
+import hello.config.MyDatasourceEnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+//@Import(MyDatasourceEnvConfig.class)
+@Import(MyDataSourceValueConfig.class)
+@SpringBootApplication(scanBasePackages = "hello.datasource")
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
