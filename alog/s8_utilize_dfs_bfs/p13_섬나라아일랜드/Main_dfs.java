179414249b58cd2ccc03f338c4151cf151ee3b41
samba-space <junkipark430@gmail.com>
2023-10-24 17:00:22 +0900

    섬나라아일랜드 dfs

--- alog/s8_utilize_dfs_bfs/p13_섬나라아일랜드/Main_dfs.java
+++ alog/s8_utilize_dfs_bfs/p13_섬나라아일랜드/Main_dfs.java
@@ -10,14 +10,13 @@ public class Main_dfs {
     int[] dirX = {0, 1, 0, -1, -1, 1, -1, 1};
     int[] dirY = {-1, 0, 1, 0, -1, -1, 1, 1};
     int[][] map;
-    int[][] visited;
+    int answer = 0;
 
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         Main_dfs T = new Main_dfs();
         T.N = in.nextInt();
         T.map = new int[T.N][T.N];
-        T.visited = new int[T.N][T.N];
 
         for (int i = 0; i < T.N; i++) {
             for (int j = 0; j < T.N; j++) {
@@ -25,38 +24,28 @@ public class Main_dfs {
                 T.map[i][j] = status;
             }
         }
-
-        System.out.println(T.bfs());
-    }
-
-    public int bfs() {
-        int answer = 0;
-        Queue<int[]> queue = new LinkedList<>();
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (map[i][j] == 1 && visited[i][j] == 0) {
-                    queue.offer(new int[]{i, j});
-                    visited[i][j] = 1;
-                    answer++;
-                    while (!queue.isEmpty()) {
-                        int size = queue.size();
-                        for (int z = 0; z < size; z++) {
-                            int[] currentPos = queue.poll();
-                            for (int k = 0; k < 8; k++) {
-                                int nextY = dirY[k] + currentPos[0];
-                                int nextX = dirX[k] + currentPos[1];
-                                if (nextX >= 0 && nextX < N && nextY >= 0 && nextY < N && visited[nextY][nextX] == 0 && map[nextY][nextX] == 1) {
-                                    queue.offer(new int[]{nextY, nextX});
-                                    visited[nextY][nextX] = 1;
-                                }
-                            }
-                        }
-                    }
+        for (int i = 0; i < T.N; i++) {
+            for (int j = 0; j < T.N; j++) {
+                if (T.map[i][j] == 1) {
+                    T.map[i][j] = 0;
+                    T.dfs(i, j);
+                    T.answer++;
                 }
+
             }
         }
 
-        return answer;
+        System.out.println(T.answer);
+    }
+
+    public void dfs(int y, int x) {
+        for (int i = 0; i < 8; i++) {
+            int nextY = y + dirY[i];
+            int nextX = x + dirX[i];
+            if (nextY >= 0 && nextY < N && nextX >= 0 && nextX < N && map[nextY][nextX] == 1) {
+                map[nextY][nextX] = 0;
+                dfs(nextY, nextX);
+            }
+        }
     }
 }
