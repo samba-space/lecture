652ed6feaa2a54be012d5fc5f8089fc0d383a92d
samba-space <junkipark430@gmail.com>
2023-11-08 20:37:57 +0900

    greedy

--- alog/s8_utilize_dfs_bfs/p14_피자배달거리/Main.java
+++ alog/s8_utilize_dfs_bfs/p14_피자배달거리/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     List<int[]> house = new ArrayList<>();
     List<int[]> pizza = new ArrayList<>();
-    int[] check;
+    int[] combination;
     int answer = Integer.MAX_VALUE;
 
     public static void main(String[] args){
@@ -26,41 +26,31 @@ public class Main {
                 }
             }
         }
-        T.check = new int[T.pizza.size()];
+        T.combination = new int[T.M];
         T.dfs(0, 0);
         System.out.println(T.answer);
     }
 
-    public void dfs(int level, int selectPizzaCount) {
-        if (selectPizzaCount > M) {
+    public void dfs(int level, int start) {
+        if (level == M) {
+            int sum = calDistance();
+            answer = Math.min(sum, answer);
             return;
         }
-        if (level == check.length) {
-            if (selectPizzaCount == M) {
-                int sum = calDistance();
-                if (answer > sum) {
-                    answer = sum;
-                }
-            }
-            return;
+        for (int i = start; i < pizza.size(); i++) {
+            combination[level] = i;
+            dfs(level + 1, i + 1);
         }
-        check[level] = 1;
-        dfs(level + 1, selectPizzaCount + 1);
-        check[level] = 0;
-        dfs(level + 1, selectPizzaCount);
     }
 
     public int calDistance() {
         int sum = 0;
         for (int[] housePosition : house){
             int minSum = Integer.MAX_VALUE;
-            for (int i = 0; i < check.length; i++) {
-                if (check[i] == 1) {
-                    int tempSum = Math.abs(housePosition[0] - pizza.get(i)[0]) + Math.abs(housePosition[1] - pizza.get(i)[1]);
-                    if (minSum > tempSum) {
-                        minSum = tempSum;
-                    }
-                }
+            for (int i = 0; i < combination.length; i++) {
+                int index = combination[i];
+                int tempSum = Math.abs(housePosition[0] - pizza.get(index)[0]) + Math.abs(housePosition[1] - pizza.get(index)[1]);
+                minSum = Math.min(tempSum, minSum);
             }
             sum += minSum;
         }
